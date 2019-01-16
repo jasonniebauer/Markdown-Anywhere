0f83de39f060dd7c0b437e16115af1b1fd0a14b1
Jason <lookwhatjasondid@gmail.com>
2019-01-15 18:08:52 -0600

    clean

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# DocHub
+# Markdown Anywhere
+Markdown Anywhere is a progressive web app powered Markdown editor.
+
+### Tech
+Markdown Anywhere uses a number of open source projects to work properly:
+* [markdown-it](https://github.com/markdown-it/markdown-it) - Markdown parser, done right.
+* [Litespeed CSS](https://github.com/jasonniebauer/litespeed-css) - minimal UI boilerplate for modern web apps
---
+++ js/app.js
@@ -0,0 +1,47 @@
+// Define constants
+const appContainer = document.getElementById('appContainer');
+const editor = document.getElementById('editor');
+const previewContainer = document.getElementById('previewContainer');
+const previewIcon = document.getElementById('previewIcon');
+
+// Define global funcs
+function updatePreview(){
+  previewContainer.innerHTML =  md.render(editor.value);
+}
+
+function togglePreview(){
+  previewIcon.classList.toggle('active');
+  editor.classList.toggle('hide');
+  previewContainer.classList.toggle('md-show');
+}
+
+// Listen for events
+previewIcon.addEventListener('click', function() {
+  togglePreview();
+});
+
+editor.addEventListener('input', function (event) {
+  updatePreview();
+});
+
+// Set default markdown
+editor.value =
+  '# DocHub\n\n' + 
+  'A web app for writing markdown.\n\n' +
+  '# Heading 1\n' +
+  '## Heading 2\n' +
+  '### Heading 3\n' +
+  '#### Heading 4\n' +
+  '##### Heading 5\n' +
+  '###### Heading 6\n\n' +
+  'Type `*Italic*` to get *Italic*\n\n' +
+  'Type `**Bold**` to get **Bold**\n\n' +
+  'Type `[Link](http://a.com)` to get a [Link](http://a.com)\n\n' +
+  'Type `![Image](http://url/a.png)` to get ![Image](http://url/a.png)\n\n' +
+  'Type `> Blockquote` to get:\n> Blockquote\n\n' +
+  '* List\n* List\n* List\n\n' +
+  '1. List\n2. List\n3. List\n\n';
+
+// Initialize
+const md = window.markdownit();
+updatePreview();
